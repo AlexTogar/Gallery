fc26cb3069217bbb81f101b160fab01f1194576e
Александр <38083808+AlexTogar@users.noreply.github.com>
2020-02-13 19:34:56 +0300

    get texture's pixels, bug with conversion texture to image

--- Assets/Initial.cs
+++ Assets/Initial.cs
@@ -1,20 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Diagnostics;
+using System.IO;
 using UnityEngine;
-
 public class Initial : MonoBehaviour
+
 {
-    public GameObject picture;
+    public Material picture;
     // Start is called before the first frame update
     void Start()
     {
-        picture = GameObject.Find("picture");
-        picture = picture.GetComponent();
+        Texture texture = picture.GetTexture("_BaseColorMap");
+        print(texture.name);
+        Texture2D texture2d = texture as Texture2D;
+        byte [] image = texture2d.EncodeToJPG();
+
+        using (MemoryStream mStream = new MemoryStream(image))
+        {
+            System.Drawing.Image.FromStream(mStream).Save("my_image.jpg");
+        }
+
+        print(texture2d.GetPixel(0, 0));
+
+        //start ruby script
+        Process.Start("cmd.exe", "/C ruby ruby_script.rb");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 }
