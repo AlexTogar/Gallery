630db9ea2ce1a6d75e65fb15dc41ca4d615225d7
Александр <38083808+AlexTogar@users.noreply.github.com>
2020-02-13 15:32:55 +0300

    add armchair, but mistake in script (getComponent)

--- Assets/Initial.cs
+++ Assets/Initial.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Initial : MonoBehaviour
 {
+    public GameObject picture;
     // Start is called before the first frame update
     void Start()
     {
-        
+        picture = GameObject.Find("picture");
+        picture = picture.GetComponent();
     }
 
     // Update is called once per frame
