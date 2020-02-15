2e5ab94189bde0f9345d6141c03c8bd4262cc0a3
Александр <38083808+AlexTogar@users.noreply.github.com>
2020-02-16 00:13:56 +0300

    switching pictures is done

--- Assets/Initial.cs
+++ Assets/Initial.cs
@@ -1,33 +1,87 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
 using UnityEngine;
+using System.Collections.Generic;
 public class Initial : MonoBehaviour
 
 {
     public Material picture;
+    //current index of showing picture
+    public int i = 1;
+    //flag of space pressing
+    public bool space_flag = false;
+    List<Texture2D> textures = new List<Texture2D>();
     // Start is called before the first frame update
     void Start()
     {
+        
         Texture texture = picture.GetTexture("_BaseColorMap");
-        print(texture.name);
         Texture2D texture2d = texture as Texture2D;
-        byte [] image = texture2d.EncodeToJPG();
+        FillImageList();
+        picture.SetTexture("_BaseColorMap", textures[0]);
 
-        using (MemoryStream mStream = new MemoryStream(image))
-        {
-            System.Drawing.Image.FromStream(mStream).Save("my_image.jpg");
-        }
-
-        print(texture2d.GetPixel(0, 0));
+        //texture2d.Resize(200, 200);
+        //texture2d.Apply();
 
         //start ruby script
-        Process.Start("cmd.exe", "/C ruby ruby_script.rb");
+        //Process.Start("cmd.exe", "/C ruby ruby_script.rb");
 
+    }
+
+    void FillImageList()
+    {
+        string currentFolderPath = System.Environment.CurrentDirectory;
+        DirectoryInfo d = new DirectoryInfo(currentFolderPath + "/Assets/" + "/pictures/");
+        FileInfo[] files = d.GetFiles("*.jpg");
+        foreach (FileInfo fileInfo in files)
+        {
+            textures.Add(LoadJPG(fileInfo));
+        }
+    }
+
+    static Texture2D LoadJPG(FileInfo filePath)
+    {
+        Texture2D tex = null;
+        byte[] fileData;
+
+        if (File.Exists(filePath.FullName))
+        {
+            fileData = File.ReadAllBytes(filePath.FullName);
+            tex = new Texture2D(2, 2);
+            tex.name = Path.GetFileNameWithoutExtension(filePath.Name);
+            tex.LoadImage(fileData); //..this will auto-resize the texture dimensions. 
+        }
+        return tex;
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        // change picture by pressing space
+        if (Input.GetKeyDown("space") && space_flag == false)
+        {
+            space_flag = true;
+            i = i + 1;
+            if (i > 3)
+            {
+                i = 1;
+            }
+            //handler of space pressing
+            /*
+            string picture_path = Application.dataPath + "/pictures/picture" + i.ToString();
+            Texture2D new_texture2d = Resources.Load(picture_path, typeof(Texture2D)) as Texture2D;
+            picture.SetTexture("_MainTex", new_texture2d);
+            */
+            
+            picture.SetTexture("_BaseColorMap", textures[i-1]);
+        }
+        else
+        {
+
+        }
+        {
+            space_flag = false;
+        }
     }
 }
--- ruby_script.rb
+++ ruby_script.rb
@@ -1,2 +1,9 @@
-puts "this is a ruby script"
-gets
+require 'mini_magick'
+require 'image_magick'
+
+
+file_path = "./Assets/pictures/picture1.jpg"
+image = MiniMagick::Image.open(file_path)
+# image.resize "10x10";
+# image.format "jpg"
+image.write "./generated_image.jpg"
