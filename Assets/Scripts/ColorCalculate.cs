cdb7d2649fb52ec1bc8a5ed00d1747ec76d16c62
Александр <38083808+AlexTogar@users.noreply.github.com>
2020-03-24 13:18:14 +0300

    add kmeans algorithm, but there is problem with NaN in Colors

---
+++ Assets/Scripts/ColorCalculate.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Color = UnityEngine.Color;
+public class ColorCalculate : MonoBehaviour
+{
+
+	public class Node
+	{
+		public Node(float dis, int num)
+		{
+			distance = dis;
+			centerNum = num;
+		}
+		public float distance { get; set; }
+		public int centerNum { get; set; }
+	}
+
+	public static List<Color> TextureToColorsList(Texture2D texture, int compressionCoef)
+	{
+		List<Color> listPixels = new List<Color>();
+		for (int i = 0;i < (int)texture.height/compressionCoef; i+=compressionCoef)
+		{
+			for (int j = 0; j < (int)texture.width/compressionCoef; j+=compressionCoef)
+			{
+				listPixels.Add(texture.GetPixel(i, j));
+			}
+
+		}
+		return listPixels;
+	}
+
+
+	public static float CalcDistance(Color colorOne, Color colorTwo)
+	{
+		Vector3 a = new Vector3(colorOne.a, colorOne.b, colorOne.g);
+		Vector3 b = new Vector3(colorTwo.a, colorTwo.b, colorTwo.g);
+		return Vector3.Distance(a, b);
+	}
+
+	public static Color CalcCenterOfMass(List<Color> colorList)
+	{
+		Vector3 result = new Vector3(0,0,0);
+		foreach(Color node in colorList)
+		{
+			result += new Vector3(node.r, node.b, node.g);
+		}
+		result = result / colorList.Count();
+
+		return new Color(result.x, result.y, result.z);
+	}
+
+	
+
+
+	public static List<Color> GetMainColors(Texture2D texture, int n = 12)
+	{
+
+		List<Color> colorList = TextureToColorsList(texture, 5);
+		List<Color> centersList = new List<Color>();
+
+		//set random c+enters position
+		for (int i = 0; i < 3; i++)
+		{
+			centersList.Add(new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f)));
+		}
+		Debug.Log("centers before algorithm processing:" + 
+			centersList[0].ToString() + "\n" + 
+			centersList[1].ToString() + "\n" +
+			centersList[2].ToString() + "\n");
+	
+		//calculate lists of pixels belonging to centers
+		List<Node> distancesList = new List<Node>();
+
+		for (int j = 0; j < n; j++)
+		{
+			List<List<Color>> listCentersPixels = new List<List<Color>>();
+			for (int i = 0; i < 3; i++)
+			{
+				listCentersPixels.Add(new List<Color>());
+			}
+
+			foreach (Color color in colorList)
+			{
+				distancesList = new List<Node>();
+
+				distancesList.Add(new Node(CalcDistance(color, centersList[0]), 0));
+				distancesList.Add(new Node(CalcDistance(color, centersList[1]), 1));
+				distancesList.Add(new Node(CalcDistance(color, centersList[2]), 2));
+
+				//sort list
+				distancesList = distancesList.OrderBy(x => x.distance).ToList();
+
+				int centerNum = distancesList[0].centerNum;
+				listCentersPixels[centerNum].Add(color);
+			}
+
+			List<Color> newCentersList = new List<Color>();
+			for (int i = 0; i < 3; i++)
+			{
+				newCentersList.Add(CalcCenterOfMass(listCentersPixels[i]));
+			}
+
+			centersList = newCentersList;
+		}
+
+		Debug.Log("centers after algorithm processing:" +
+			centersList[0].ToString() + "\n" +
+			centersList[1].ToString() + "\n" +
+			centersList[2].ToString() + "\n");
+
+		return centersList;
+	}
+}
---
+++ Assets/Scripts/Initial.cs
@@ -0,0 +1,277 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityTemplateProjects;
+using Color = UnityEngine.Color;
+
+public class Initial : MonoBehaviour
+{
+    public Material picture;
+    public Material mainColorOne;
+    public Material mainColorTwo;
+    public Material mainColorThree;
+    public GameObject directionalLight;
+    public GameObject skyAndFogVolume;
+    public GameObject canvas;
+    public GameObject viewPortContent;
+    public GameObject mainMenu;
+    public GameObject selectPictureMenu;
+    public GameObject changeEnvironmentMenu;
+    public GameObject RowPrefab;
+    public GameObject mainCamera;
+    protected System.Random rnd = new System.Random();
+    //current index of showing picture
+    protected int i = 0;
+    //flags of buttons pressing
+    protected bool lFlag = false;
+    protected bool jFlag = false;
+    protected bool f1Flag = false;
+    //textures list, which will be filled by FillTexturesList() method, called in Start() method
+    protected List<Texture2D> textures = new List<Texture2D>();
+    //protected List<System.Drawing.Bitmap> bitmaps = new List<Bitmap>();
+    //===================== HELPFUL ADDITIONAL OWN METHODS ===========================
+
+    //Deactive all menu to show one from them after that
+    public void DeactiveAllMenu()
+    {
+        mainMenu.SetActive(false);
+        selectPictureMenu.SetActive(false);
+        changeEnvironmentMenu.SetActive(false);
+    }
+    //fill textures list from /Assets/pictures/ folder with .jpg extension
+    void FillTexturesList()
+    {
+        string currentFolderPath = System.Environment.CurrentDirectory;
+        DirectoryInfo d = new DirectoryInfo(currentFolderPath + "/Assets/" + "/pictures/");
+        FileInfo[] files = d.GetFiles("*.jpg");
+        foreach (FileInfo fileInfo in files)
+        {
+            textures.Add(LoadJPG(fileInfo));
+        }
+        textures = textures.OrderBy(o => o.name).ToList();
+    }
+
+    //Convertion from .jgp to texture
+    static Texture2D LoadJPG(FileInfo filePath)
+    {
+        Texture2D tex = null;
+        byte[] fileData;
+
+        if (File.Exists(filePath.FullName))
+        {
+            fileData = File.ReadAllBytes(filePath.FullName);
+            tex = new Texture2D(2, 2);
+            tex.name = Path.GetFileNameWithoutExtension(filePath.Name);
+            tex.LoadImage(fileData); //..this will auto-resize the texture dimensions. 
+        }
+        return tex;
+    }
+
+    public void ChangeEnvironmentByTexture(Texture2D texture)
+    {
+        Color mainColor = GetMainColor(texture);
+        ChangeEnvironmentColor(mainColor);
+        SetDirectionalLightPosition(mainColor);//emtpy function yet
+
+    }
+
+    //rewrite it
+    public Color GetMainColor(Texture2D texture)
+    {
+        return texture.GetPixel((int)(Random.Range(0, 1f) * texture.height), (int)(Random.Range(0, 1f) * texture.width));
+    }
+
+    public void SetLightColor(Color color)
+    {
+        directionalLight.GetComponent<Light>().color = color;
+
+    }
+
+    public void SetFogColor(Color color)
+    {
+        RenderSettings.fogColor = color;  //doesn't work yet
+    }
+
+    public void SetDirectionalLightPosition(Color color)
+    {
+        
+    }
+
+
+    public void ChangeEnvironmentColor(Color color)
+    {
+        SetLightColor(color);
+        SetFogColor(color);
+    }
+
+    public void setMainColors(List<Color> colors)
+    {
+        mainColorOne.SetColor("_BaseColor", colors[0]);
+        mainColorTwo.SetColor("_BaseColor", colors[1]);
+        mainColorThree.SetColor("_BaseColor", colors[2]);
+
+    }
+
+    public void SetRandomEnvironmentColor()
+    {
+        ChangeEnvironmentColor(new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f)));
+    }
+
+    public void ResumeOrPauseGame()
+    {
+        if (canvas.active)
+        {
+            canvas.SetActive(false);
+        }
+        else
+        {
+            DeactiveAllMenu();
+            //show exactly main menu
+            mainMenu.SetActive(true);
+            canvas.SetActive(true);
+        }
+    }
+
+    public void SetPictureByName(string pictureName)
+    {
+        // Texture2D newTexture = new Texture2D(1000, 1000);
+        int index = 0;
+        foreach (Texture2D texture in textures)
+        {
+
+            if (texture.name == pictureName)
+            {
+                picture.SetTexture("_BaseColorMap", texture);
+                setMainColors(ColorCalculate.GetMainColors(texture, 15));
+                i = index;
+                break;
+            }
+            index++;
+        }
+    }
+
+    public void SetNextPicture()
+    {
+        i += 1;
+        if (i > textures.Count - 1) { i = 0; }
+        picture.SetTexture("_BaseColorMap", textures[i]);
+        ChangeEnvironmentByTexture(textures[i]);
+    }
+
+    public void SetPreviousPicture()
+    {
+        i -= 1;
+        if (i < 0) { i = textures.Count - 1; }
+        picture.SetTexture("_BaseColorMap", textures[i]);
+        ChangeEnvironmentByTexture(textures[i]);
+    }
+
+    public void QuitGame()
+    {
+        //for application
+        Application.Quit();
+        //for unity editor
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+
+
+    //===================== BUILT-IN METHODS ===========================
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Hide main menu
+        canvas.SetActive(false);
+
+
+        FillTexturesList();
+
+        //check up new images in the picture folder and computing main colors
+        /*
+        foreach(Texture2D texture in textures)
+        {
+            var md5 = System.Security.Cryptography.MD5.Create();
+            byte[] hash = md5.ComputeHash(filesArray[k]);
+
+            if (hash != hashArrayFromFile[k])
+            {
+
+            }
+
+            k++;
+        }
+        */
+
+        //Set default picture and environment
+        picture.SetTexture("_BaseColorMap", textures[0]);
+        ChangeEnvironmentByTexture(textures[0]);
+        //allocate space in the Content object for header and rows
+        viewPortContent.GetComponent<RectTransform>().sizeDelta = new Vector2(0, 100 * textures.Count + 70);
+        int j = 0;
+        //create select list of pictures
+        foreach (Texture2D texture in textures)
+
+        {
+            
+            GameObject Row = Instantiate(RowPrefab);
+            Row.transform.SetParent(viewPortContent.transform);
+            Row.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, -120 - (100*j));
+            Row.name = "Row" + i.ToString();
+            //set image name
+            Row.GetComponentsInChildren<Text>()[0].text = texture.name;
+            //set preview image
+            Row.GetComponentsInChildren<Image>()[0].sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+            //set OnClick handler
+            Row.GetComponentsInChildren<Button>()[0].onClick.AddListener(() => { SetPictureByName(texture.name); ChangeEnvironmentByTexture(texture); });
+            j += 1;
+        }
+
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //handler of the l pressing (next picture)
+        if (Input.GetKeyDown("l") && lFlag == false)
+        {
+            lFlag = true;
+            SetNextPicture();
+        }
+        else
+        {
+            lFlag = false;
+        }
+
+        //handler of the j pressing (prev picture)
+        if (Input.GetKeyDown("j")&& jFlag == false)
+        {
+            jFlag = true;
+            SetPreviousPicture();
+        }
+        else
+        {
+            jFlag = false;
+        }
+
+        //handler of the f1 pressing (Pause/Resume)
+        if (Input.GetKeyDown(KeyCode.F1) && f1Flag == false)
+        {
+            f1Flag = true;
+            ResumeOrPauseGame();
+        }
+        else
+        {
+            f1Flag = false;
+        }
+
+
+    }
+}
